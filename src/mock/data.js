c25425a0208b1b088c6e39339cd7b8c570552b87
poppito <underseer@gmail.com>
2020-11-01 17:40:14 +1100

    initial

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,7 +10,7 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: 'Hello, friend - my name is',
-  name: 'Harsh Overseer,',
+  name: 'Harsh Overseer',
   subtitle: 'Nice to meet you.',
   cta: '',
 };
