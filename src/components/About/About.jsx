7d39344cd29111a6491a8be008b86b92e03279a6
poppito <underseer@gmail.com>
2020-11-01 20:27:14 +1100

    add projects

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ import PortfolioContext from '../../context/context';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { img, paragraphOne, paragraphTwo, paragraphThree, blog } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -48,15 +48,15 @@ const About = () => {
                 <p className="about-wrapper__info-text">
                   {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
                 </p>
-                {resume && (
+                {blog && (
                   <span className="d-flex mt-3">
                     <a
                       target="_blank"
                       rel="noopener noreferrer"
                       className="cta-btn cta-btn--resume"
-                      href={resume}
+                      href={blog}
                     >
-                      Resume
+                      Blog
                     </a>
                   </span>
                 )}
--- src/mock/data.js
+++ src/mock/data.js
@@ -9,57 +9,57 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: 'Hello, friend - my name is',
+  title: 'Hello - my name is',
   name: 'Harsh Overseer',
   subtitle: 'Nice to meet you.',
-  cta: '',
+  cta: 'More.',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'I am a Software Engineer based in Melbourne, Australia. I like building stuff - mobile apps and games mainly - Android and iOS.',
+  paragraphOne: 'I am a Software Engineer based in Melbourne, Australia. I like building stuff - mobile apps and games mainly, natively on Android and iOS.',
   paragraphTwo: 'I write code in Kotlin (or Java), Swift and on darker days, Javascript. Because I am obsessed with fixing things, sometimes I get lured into DevOps too.',
-  paragraphThree: 'I can often be found on twitter where I rant about Android, iOS and DevOps, the current state of Australian Politics or complain about the NBN, Telstra and Metro trains (aussie whinge staples)',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphThree: 'I can often be found on twitter where I rant about code, the current state of Australian Politics or complain about the NBN, Telstra and Metro trains (aussie whinge staples)',
+  blog: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'paradox.png',
+    title: 'Particle Paradox',
+    info: 'Particle Paradox is a game about a Particle\'s journey in multiple universes.',
+    info2: 'Available on both Android and iOS',
+    url: 'https://apps.apple.com/au/app/particle-paradox/id1536898085',
+    repo: 'https://play.google.com/store/apps/details?id=io.embry.crowley&hl=en_AU&gl=US', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'huxtable.png',
+    title: 'The Huxtable Saga',
+    info: 'A 2d side scrolling adventure game with plenty of puzzles!',
+    info2: 'Available on both Android and iOS',
+    url: 'https://apps.apple.com/au/app/the-huxtable-saga/id1498415989',
+    repo: 'https://play.google.com/store/apps/details?id=io.embry.huxtable&hl=en_AU&gl=US', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'strayamate.png',
+    title: 'Aussie slang, pronunciation sounds and places',
+    info: 'The #1 Aussie slang app on Android and iOS',
+    info2: 'Explore Aussie slang with recorded pronunciations, a Slangman game and more. On iOS it also has a game on Apple Watch',
+    url: 'https://apps.apple.com/au/app/strayamate-aussie-slang-more/id1363054907#?platform=iphone',
+    repo: 'https://play.google.com/store/apps/details?id=strayanslangapp.noni.com.strayanslangapp&hl=en_AU&gl=US', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Questions? Comments? Compliments? Wanna get in touch?',
+  btn: 'Say hello',
+  email: 'harsh@embry.io',
 };
 
 // FOOTER DATA
@@ -68,22 +68,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/overjeer',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'instagram',
+      url: 'https://instagram.com/embryio',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://linkedin.com/in/hoverseer',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/poppito',
     },
   ],
 };
