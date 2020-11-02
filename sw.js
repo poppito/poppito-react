4153e9c6490b5d574e89c0b81974bee6bc8c61ab
poppito <underseer@gmail.com>
2020-11-02 13:49:06 +1100

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b43dc4ccc121b047c0c7.js"
+    "url": "webpack-runtime-412c25c40ba4593113a3.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
   },
   {
-    "url": "app-ffbb4ddffd417090c381.js"
+    "url": "app-587303dd14cd85e28540.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7f197ae872154e2bd1cf9c098cb55fd4"
+    "revision": "08f5101d156dbaab2705426cdc9660f0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,14 +48,14 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "15c3e02ad3bb009cd716bd7281f5d449"
+    "revision": "e96ec4cf02244faa3a4ccf654e33aad3"
   },
   {
     "url": "polyfill-4b1e8b7e04c413f9811f.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "6e06e2dc973171749c8eb546f3d72637"
+    "revision": "44f6310c60434cbde696ebd0a1ad0b44"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -142,12 +142,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/poppito-react`), ``)
+  pathname = pathname.replace(new RegExp(`^/poppito`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/poppito-react/app-ffbb4ddffd417090c381.js`))) {
+  if (!resources || !(await caches.match(`/poppito/app-587303dd14cd85e28540.js`))) {
     return await fetch(event.request)
   }
 
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/poppito-react/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/poppito/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
