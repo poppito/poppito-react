4407e53c0d667ccf644acb8c5a6a83ece0474def
poppito <underseer@gmail.com>
2020-11-04 20:18:29 +1100

    add changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,13 +32,5 @@ module.exports = {
         icon: 'src/images/favicon.png',
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/markdown-pages`,
-        name: `markdown-pages`,
-      },
-    },
-    `gatsby-transformer-remark`,
   ],
 };
