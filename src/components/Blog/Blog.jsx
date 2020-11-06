28a4af07db95114d1b520904197f0bdbc15ee800
poppito <underseer@gmail.com>
2020-11-06 19:41:21 +1100

    remove unneeded button

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,10 +4,6 @@ const Blog = () => {
 
   return (
     <section id="blog">
-        <a className="cta-btn cta-btn--resume"
-        href={'/blog'}
-        >{"Blog"}
-        </a>
         <h1>JIGUP PAPATUN JIGA PAPATUN</h1>
     </section>
   );
