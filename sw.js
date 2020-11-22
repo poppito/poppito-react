989f8e676afe05c261b847dbfa0f0b76eb4fe4f9
poppito <underseer@gmail.com>
2020-11-22 13:41:46 +1100

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1d45a698bbc2c57d2fb6.js"
+    "url": "webpack-runtime-7ef8b0f305a8301b7183.js"
   },
   {
-    "url": "styles.479bc09e20735f1307c5.css"
+    "url": "styles.f7fa25d1b04346c79a8c.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "da689ac9dfa4faa8685b623e17f45b35"
+    "revision": "7caca7cee12a824dfbbb2a02bb3f70ec"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
