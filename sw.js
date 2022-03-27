cef8dfb8404d9f3a1be946a1c5cf3de480b72561
harshoverseer <underseer@gmail.com>
2022-03-27 21:40:00 +1100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-edd649ca7eaaa88b0b20.js"
+    "url": "webpack-runtime-9aaf8103de804b717244.js"
   },
   {
     "url": "framework-707a8a8db8797361392f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1b223c6f0581dc9e6bcc0016f4094647"
+    "revision": "471f8266364a7411970d38e05a55974f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
